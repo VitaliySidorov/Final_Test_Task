602c29336ead9b569f3fb1bcdec177c8ce73c63b
Vitaliy <vitaliysidorov@yandex.ru>
2023-02-24 16:19:25 +0500

    output method modification

--- Program.cs
+++ Program.cs
@@ -7,49 +7,51 @@
 // [“Russia”, “Denmark”, “Kazan”] → []
 
 Console.Clear();
-Console.WriteLine("Программа формирования массива строк из исходного массива, длина которых не более 3 символов.\n");
+string taskText = "Программа формирования массива строк из исходного массива, длина которых не более 3 символов.\n";
+Console.WriteLine(taskText);
+File.WriteAllText("output.txt", taskText);
 
 // Создание и инициализация строковых массивов
 string[] array1 = { "Hello", "2", "world", ":-)" };
 string[] array2 = { "1234", "1567", "-2", "computer science" };
 string[] array3 = { "Russia", "Denmark", "Kazan" };
 
-int maxWordLength = 3; // Инициализация переменной для ограничения по количеству символов в слове
+int maxLength = 3; // Инициализация переменной для ограничения по количеству символов в слове
 
-OutputArray(array1);
-OutputArray(array2);
-OutputArray(array3);
+OutputArrays(array1);
+OutputArrays(array2);
+OutputArrays(array3);
 
+void OutputArrays(string[] array) // Метод результирующего вывода массивов
+{
+    string resultString = (PrintArray(array) + "   ->   " +
+                           PrintArray(SelectRequiredArrayElements(array)) +
+                           "\n"); // Формирование строки как в задании
+    Console.WriteLine(resultString);
+    File.AppendAllText("output.txt", resultString); // Добавление результатов в файл
+}
 
-string[] SelecRequiredArrayElements(string[] original) // Метод отбора подходящих условию элементов массива
+string PrintArray(string[] array) // Метод приведения массива к виду [" ", ..., " "]
+{
+    string formatArray = ("[\"" + string.Join("\", \"", array) + "\"]");
+    return formatArray;
+}
+
+string[] SelectRequiredArrayElements(string[] original) // Метод отбора подходящих условию элементов массива
 {
     int length = original.Length;
-    string[] result = new string[length]; // Создание результирующего массива с длиной равной исходному
-    int count = 0; // Инициализация счетчика подходяших элементов
+    string[] result = new string[length];   // Создание результирующего массива с длиной равной исходному
+    int count = 0;                          // Инициализация счетчика подходяших элементов
 
     for (int i = 0; i < length; i++)
     {
-        if (original[i].Length <= maxWordLength)
+        if (original[i].Length <= maxLength)
         {
-            result[count] = original[i]; // Запись подходящего элемента в результирующий массив
+            result[count] = original[i];    // Запись подходящего элемента в результирующий массив
             count++;
         }
     }
-    Array.Resize(ref result, count); // Сокращение результирующего массива
+    Array.Resize(ref result, count);        // Сокращение результирующего массива
 
     return result;
-}
-
-void PrintArray(string[] array) // Метод вывода массива в консоль
-{
-    Console.Write("[\"" + string.Join("\", \"", array) + "\"]");
-}
-
-void OutputArray(string[] array)
-{
-    PrintArray(array);
-    Console.Write(" -> ");
-    string[] resoltArray = SelecRequiredArrayElements(array);
-    PrintArray(resoltArray);
-    Console.WriteLine("\n");
 }
